93f4b2e35d287d1ee14ea8379a37c67c7c21727a
matte4reale <filippot222@gmail.com>
2025-07-20 10:29:21 +0200

    Update Notiziario.js

--- plugins/Notiziario.js
+++ plugins/Notiziario.js
@@ -1,5 +1,3 @@
- // notiziario.js import fs from 'fs';
-
 const categories = JSON.parse(fs.readFileSync('./rss_sources.json', 'utf8'));
 
 let handler = async (m, { conn }) => { const buttons = Object.keys(categories).map(cat => ({ buttonId: .selectNews ${cat}, buttonText: { displayText: cat.charAt(0).toUpperCase() + cat.slice(1) }, type: 1 })); await conn.sendMessage(m.chat, { text: '📡 Seleziona la categoria di news:', footer: 'Notiziario Generale', buttons, headerType: 1 }, { quoted: m }); };
